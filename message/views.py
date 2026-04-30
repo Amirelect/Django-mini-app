4e90c1a31e3bdd323c8d9393515ecb51ea7168fc
Amirelect <amirelect.graph@gmail.com>
2026-04-30 16:26:02 +0330

    complate developer branch to same main branch

--- message/views.py
+++ message/views.py
@@ -1,6 +1,19 @@
 
-from django.views.generic import TemplateView
+from django.shortcuts import render
+from django.views import View
+from django.views.generic import TemplateView, ListView
+from .models import Message
 
-class MessageView(TemplateView):
+# def messageView(request):
+#     return render(request, 'home.html')
+
+# class MessageView(View):    
+#     def get(self, request):
+#         return render(request, 'home.html')
+
+# class MessageView(TemplateView):
+#     template_name = 'home.html'
+
+class MessageView(ListView):
+    model = Message    
     template_name = 'home.html'
-    
